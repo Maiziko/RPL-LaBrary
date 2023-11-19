e1fffbadaacc1acaee23f2d8b94339c3b67aa26e
Gyan Maiziko <18221008@std.stei.itb.ac.id>
2023-11-20 00:46:33 +0700

    Sedikit lebih responsive login pagenya

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabase';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
+const LaBraryLogo = () => (
+  <Image
+    src="/images/libraryLogo.svg"
+    alt="My Image"
+    width={200}
+    height={60}
+    className="left-10 top-10"
+  />
+);
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -34,57 +44,69 @@ const Login: React.FC = () => {
   };
   
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 h-full w-full">
-    <div className='md:h-full' style={{ backgroundImage: "url('/images/LibraryImage.png')"}}>
-    </div>
-    <div className='bg-[#42898C] w-full h-full'>
-        <div className='max-w-[400] w-full mx-auto p-4'>
-            <div className='my-4 py-auto'>
-                <h2 className='text-5xl font-bold font-poppins text-center py-auto my-auto text-white'>Welcome</h2>
+    <div className="flex min-h-screen">
+        <div className="flex-1 bg-[#42898C] relative backdrop-brightness-50" style={{background: 'url(/images/labraryLoginPict.png)', backgroundSize: 'cover',}}>
+            <div className="left-10 p-10 backdrop-brightness-90">
+              <LaBraryLogo />
             </div>
-            <div className='flex-col py-8'>
-                <div className='flex flex-col py-2'>
-                    <div className='px-4 md:px-12'>
-                        <label className='text-white text-2xl font-poppins'>Email:</label>
-                    </div>
-                    <div className='px-4 md:px-12'>
-                        <input 
-                            type="email"
-                            placeholder='Type your email'
-                            className='mx-auto text-black text-2xl px-4 md:px-8 font-poppins py-4 w-full my-2 outline-none focus:outline-none rounded-lg' 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>   
-                </div>
-                <div className='flex flex-col py-2'>
-                    <div className='px-4 md:px-12'>
-                        <label className='text-white text-2xl font-poppins'>Password:</label>
-                    </div>
-                    <div className='px-4 md:px-12'>
-                        <input 
-                            type="Password"
-                            placeholder='Type your password'
-                            className='mx-auto text-black text-2xl px-4 md:px-8 font-poppins py-4 w-full my-2 outline-none focus:outline-none rounded-lg' 
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                </div>
-                <div className='px-4 md:px-12 flex justify-between'>
-                    <p className='flex items-center text-white text-lg'>
-                        <input className='mx-auto' type="checkbox" />Remember Me
-                    </p>
-                    <p className='text-white underline text-lg'>Forgot Password?</p>
-                </div>
-                <div className='py-4 my-6 flex justify-center items-center'>
-                    <button onClick={handleLogin} className='mx-auto text-white text-3xl font-poppins px-8 md:px-16 py-4 bg-[#C86F43] outline-none focus:outline-none rounded-lg'>Login</button>
-                </div>
+            
+            <div className="absolute left-10 right-10 bottom-10 rounded-lg backdrop-brightness-50 p-10 text-white font-poppins">
+                <h3 className='mb-1 text-3xl font-medium'>
+                    Selamat Datang,
+                </h3>
+                <span className="text-3xl font-medium">Temukan Bukumu di <b>LaBrary!</b></span>
             </div>
         </div>
+        <div className="flex-1 flex items-center justify-center bg-[#42898C]">
+            
+                      <div className="w-full mx-auto p-4 flex flex-col items-center justify-center h-screen">
+      <div className="my-4">
+        <h2 className="text-5xl font-bold font-poppins text-center text-white">Welcome</h2>
+      </div>
+      <div className="flex-col py-8">
+        <div className="flex flex-col py-2 w-full md:w-auto">
+          <div className="px-4 md:px-12">
+            <label className="text-white text-2xl font-poppins">Email:</label>
+          </div>
+          <div className="px-4 md:px-12">
+            <input
+              type="email"
+              placeholder="Type your email"
+              className="mx-auto text-black text-2xl px-4 md:px-8 lg:px-12 font-poppins py-4 w-full my-2 outline-none focus:outline-none rounded-lg"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col py-2 w-full md:w-auto">
+          <div className="px-4 md:px-12">
+            <label className="text-white text-2xl font-poppins">Password:</label>
+          </div>
+          <div className="px-4 md:px-12">
+            <input
+              type="password"
+              placeholder="Type your password"
+              className="mx-auto text-black text-2xl px-4 md:px-8 lg:px-12 font-poppins py-4 w-full my-2 outline-none focus:outline-none rounded-lg"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="px-4 md:px-12 flex justify-between">
+          <p className="flex items-center text-white text-lg">
+            <input className="mx-auto" type="checkbox" />Remember Me
+          </p>
+          <p className="text-white underline text-lg">Forgot Password?</p>
+        </div>
+        <div className="py-4 my-6 flex justify-center items-center">
+          <button onClick={handleLogin} className="mx-auto text-white text-3xl font-poppins px-8 md:px-16 py-4 bg-[#C86F43] outline-none focus:outline-none rounded-lg">Login</button>
+        </div>
+      </div>
     </div>
-</main>
 
+
+        </div>
+    </div>
   )
     
 }
