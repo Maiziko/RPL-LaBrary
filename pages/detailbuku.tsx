47257921162857a863d15fb00042dc3734045868
Regine <18221025@std.stei.itb.ac.id>
2023-11-20 19:13:39 +0700

    detail buku page front end

---
+++ pages/detailbuku.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { supabase } from '../supabase';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+const detailBuku: React.FC = () => {
+    return(
+        
+    );
+};
+
+export default detailBuku;
