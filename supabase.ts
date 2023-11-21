c64973036c4d85e54a078ec37616988679be9d4f
Regine <18221025@std.stei.itb.ac.id>
2023-11-22 00:15:00 +0700

    supabase for koleksi buku

--- supabase.ts
+++ supabase.ts
@@ -1,11 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; // Gantilah dengan URL proyek Anda
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; // Gantilah dengan kunci anonim proyek Anda
-const supabase = supabaseUrl && supabaseKey ? createClient(supabaseUrl, supabaseKey) : null;
-
-if (!supabase) {
-    console.error("supabaseUrl dan supabaseKey tidak didefinisikan.");
-  }
+const supabase: SupabaseClient | null = supabaseUrl && supabaseKey ? createClient(supabaseUrl, supabaseKey) : null;
 
 export { supabase };
