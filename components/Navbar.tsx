e5a918a1a41a5831245831027d113ca2b528ba33
han03fiq <18221012@std.stei.itb.ac.id>
2023-11-21 23:53:25 +0700

    front end aja

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,29 +39,29 @@ const ProfilePicture = () => (
 
 const Navbar = () => {
     return(
-            <div>
+        <div>
         {/* <!-- Bagian Awal Component Navigasi Bar --> */}
-            <div className="relative min-h-screen bg-cover bg-center rounded-b-[20px]" style={{ backgroundImage: "url('/images/backgroundLibrary.png')", width: "1280px", height: "150px" }}>
+          <div className="relative min-h-screen bg-cover bg-center rounded-b-[20px]" style={{ backgroundImage: "url('/images/backgroundLibrary.png')", width: "1280px", height: "150px" }}>
             <div className="absolute bottom-10 left-0 text-left p-8 text-white" style={{ paddingLeft: '150px' }}>
-                <h1 className="text-4xl">Temukan Bukumu <br /> <p className="text-4xl">di <b>LaBrary</b></p></h1>
+              <h1 className="text-4xl">Temukan Bukumu <br /> <p className="text-4xl">di <b>LaBrary</b></p></h1>
             </div>
-                <nav className="fixed top-0 left-0 right-0 bg-strong-green p-3">
-                    <div className="container">
-                        <div className="flex justify-around justify-items-center">
-                            {/* Tombol Burger Menu */}
-                            <button className="text-white text-4xl cursor-pointer" id="burger-menu">
-                                <ImageComponent/>
-                            </button>
-                                <LaBraryLogo />
-                            <div className="relative w-96 h-10 pl-4 flex items-center rounded-full px-4 duration-300 cursor-pointer bg-white">
-                                <SearchImage />
-                                <input className="text-[15px] ml-4 w-full bg-transparent focus:outline-none" placeholder="Cari di LaBrary..." />
-                            </div>
-                            <ProfilePicture/>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+            <nav className="fixed top-0 left-0 right-0 bg-strong-green p-3">
+              <div className="container">
+                <div className="flex justify-around justify-items-center">
+                  {/* Tombol Burger Menu */}
+                  <button className="text-white text-4xl cursor-pointer" id="burger-menu">
+                    <ImageComponent/>
+                  </button>
+                    <LaBraryLogo />
+                  <div className="relative w-96 h-10 pl-4 flex items-center rounded-full px-4 duration-300 cursor-pointer bg-white">
+                    <SearchImage />
+                    <input className="text-[15px] ml-4 w-full bg-transparent focus:outline-none" placeholder="Cari di LaBrary..." />
+                  </div>
+                    <ProfilePicture/>
+                </div>
+              </div>
+            </nav>
+          </div>
         </div>
     );
 };
--- pages/listpeminjaman.tsx
+++ pages/listpeminjaman.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import CardListPeminjaman from '../components/CardListPeminjaman';
@@ -13,24 +14,64 @@ const Arrowleft = () => (
   />
 );
 
-const Page = () => {
-  // Mark the parent component as a client component
-  return (
-    <main className="bg-soft-green">
-          <Navbar/>
-          {/* <!-- Bagian Akhir Component Navigasi Bar --> */}
-          <Sidebar/>
-        <div className="flex py-10" style={{ gridTemplateColumns: 'repeat(2, minmax(0, 500px))', paddingTop: '24px', paddingLeft: '106px', paddingBottom: '24px' }}>
-            <button className="bg-gray-green hover:bg-white py-4 px-3 rounded-full cursor-pointer top-5 left-4">
-                <Arrowleft/>
-            </button>
-            <div className="px-[30px]">
-                <span className="text-4xl font-semibold text-strong-green">List Peminjaman</span>
-            </div>
-        </div>
-        <CardListPeminjaman/>
-    </main>
-  );
-};
 
-export default Page;
+const listPeminjaman: React.FC = () => {
+  return (
+    <div className='font-poppins'>
+      <div>
+        <img src="/images/BackgroundLabrary.png" className='w-full h-full' alt="gambar background"/>
+      </div>
+        
+      <div className='flex pt-8 pb-4'>
+        <div className='pl-9 pr-5 text-xl flex items-center justify-between'>
+          <Link href="/koleksibuku">
+            <img src="/icon/BackButton.png" alt="" className='w-[54px] h-[54px]'/>
+          </Link>
+        </div>
+        <div className='text-[#426E6D] font-bold flex items-center' style={{fontSize: '40px'}}>List Peminjaman</div>
+      </div>
+
+      <div className='flex'>
+        <div className='mt-5 mb-8'>
+          {[1, 2, 3, 4, 5].map((index) => (
+            <div key={index} className='flex mt-5 ml-[70px]'>
+              <div>
+                <input className='m-2 w-[40px] h-[40px] my-[90px] mr-5' type="checkbox" />
+              </div>
+              <div className='rounded-lg border-2 border-slate-200 shadow-md w-[600px] flex'>
+                <div className='bg-blue-300 w-[159px] h-[203px] my-3 ml-3 rounded-lg'></div>
+                <div className='pl-[18px] w-[400px]'>
+                  <div className='pt-[7px] font-bold' style={{fontSize: '32px'}}>The Lord of the Rings</div>
+                  <div className='pt-[7px]' style={{fontSize: '20px'}}>J.R.R. Tolkien</div>
+                  <div className='text-[#9E9FA1]  w-[350px]' style={{fontSize: '16px'}}>Menyajikan epik yang menggambarkan</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className='mt-5 rounded-lg border-2 border-slate-200 mx-auto shadow-md w-[400px] h-[300px]'>
+          <div className='text-[#426E6D] text-center items-center justify-center pt-4' style={{fontSize: '28px'}}>Ringkasan Peminjaman
+            <div className='flex'>
+              <div className='pt-7 pl-6 text-left text-[#426E6D] w-[200px]' style={{fontSize: '20px'}}>Total Buku</div>
+              <div className='pt-7 pr-6 text-[#426E6D] text-right w-[200px]' style={{fontSize: '20px'}}>x</div>
+            </div>
+            <div className='flex'>
+              <div className='pl-6 text-left text-[#426E6D] w-[200px]' style={{fontSize: '20px'}}>Tanggal Pinjam</div>
+              <div className='pr-6 text-[#426E6D] text-right w-[200px]' style={{fontSize: '20px'}}>dd/mm/yyyy</div>
+            </div>
+            <div className='flex'>
+              <div className='pl-6 text-left text-[#426E6D] w-[200px]' style={{fontSize: '20px'}}>Tanggal Kembali</div>
+              <div className='pr-6 text-[#426E6D] text-right w-[200px]' style={{fontSize: '20px'}}>dd/mm/yyyy</div>
+            </div>
+          </div>
+          <div className='text-center items-center justify-center'>
+            <button className='w-[126px] text-white py-3 mt-10 mb-4 bg-[#C86F43] rounded-lg' style={{fontSize: '20px'}}>Pinjam</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default listPeminjaman;
