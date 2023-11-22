fdb15ac4b98adb22850a473e4ba59eda6812ba7a
han03fiq <18221012@std.stei.itb.ac.id>
2023-11-22 15:34:47 +0700

    sudah bisa ngambil backend, tapi masih dari buku

--- pages/listpeminjaman.tsx
+++ pages/listpeminjaman.tsx
@@ -1,16 +1,41 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import Sidebar from '../components/Sidebar';
-import Navbar from '../components/Navbar';
-import CardListPeminjaman from '../components/CardListPeminjaman';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { format, addDays } from 'date-fns';
+import { supabase } from '../supabase';
 // ./pages/index.tsx or ./pages/_app.tsx
-
+interface Buku {
+  judul: string;
+  penulis: string;
+  deskripsi: string;
+  coverbuku: string;
+}
 
 const ListPeminjaman: React.FC = () => {
-  const [checkboxStatus, setCheckboxStatus] = useState([false, false, false, false, false]);
+  const [daftarBuku, setDaftarBuku] = useState<Buku[]>([]);
+  const [checkboxStatus, setCheckboxStatus] = useState<boolean[]>(Array().fill(false));
 
+  useEffect(() => {
+    const fetchBookData = async () => {
+      try {
+        const { data, error } = await supabase?.from('buku').select('coverbuku, judul, penulis, deskripsi') || {};
+    
+        console.log('Data:', data); // Check this log
+        console.log('Error:', error); // Check this log
+    
+        if (error) {
+          console.error('Error fetching book data:', error.message);
+          return;
+        }
+    
+        // Tetapkan data buku ke dalam state
+        setDaftarBuku(data || []);
+      } catch (error) {
+        console.error('Error fetching book data:', (error as any).message);
+      }
+    };
+    fetchBookData();
+  }, []); // Dependency array
+  
   const handleCheckboxChange = (index: number) => {
     setCheckboxStatus((prevStatus) => {
       const newStatus = [...prevStatus];
@@ -44,17 +69,22 @@ const ListPeminjaman: React.FC = () => {
 
       <div className='flex'>
         <div className='mt-5 mb-8'>
-          {[1, 2, 3, 4, 5].map((index) => (
+          {daftarBuku.map((buku, index) => (
             <div key={index} className='flex mt-5 ml-[70px]'>
               <div>
-                <input className='m-2 w-[40px] h-[40px] my-[90px] mr-5' type="checkbox" onChange={() => handleCheckboxChange(index - 1)}/>
+                <input
+                  className='m-2 w-[40px] h-[40px] my-[90px] mr-5'
+                  type="checkbox"
+                  onChange={() => handleCheckboxChange(index)}
+                  checked={checkboxStatus[index]}
+                />
               </div>
               <div className='rounded-lg border-2 border-slate-200 shadow-md w-[600px] flex'>
                 <div className='bg-blue-300 w-[159px] h-[203px] my-3 ml-3 rounded-lg'></div>
                 <div className='pl-[18px] w-[400px]'>
-                  <div className='pt-[7px] font-bold' style={{fontSize: '32px'}}>The Lord of the Rings</div>
-                  <div className='pt-[7px]' style={{fontSize: '20px'}}>J.R.R. Tolkien</div>
-                  <div className='text-[#9E9FA1]  w-[350px]' style={{fontSize: '16px'}}>Menyajikan epik yang menggambarkan</div>
+                  <div className='pt-[7px] font-bold' style={{fontSize: '32px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}}>{buku.judul}</div>
+                  <div className='pt-[7px]' style={{fontSize: '20px'}}>{buku.penulis}</div>
+                  <div className='text-[#9E9FA1]  w-[350px] overflow-hidden' style={{fontSize: '16px', textOverflow: 'ellipsis', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 2}}>{buku.deskripsi}</div>
                 </div>
               </div>
             </div>
