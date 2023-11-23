190435be4f45cd1f3baca9f0c547f3861369eddf
Regine <18221025@std.stei.itb.ac.id>
2023-11-23 19:35:17 +0700

    loading

--- pages/detailbuku.tsx
+++ pages/detailbuku.tsx
@@ -4,91 +4,94 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 
+
 interface BukuDetail {
   judul: string;
   penulis: string;
   penerbit: string;
   deskripsi: string;
   availability: string;
-  image: string;
+  coverbuku: string;
   kategori: string;
   tahunterbit: string;
 }
+
 interface Props {
-  buku: BukuDetail;
+  bukuId: number;
 }
 
-const DetailBuku: React.FC<Props>= ({ buku }) => {
+const DetailBuku: React.FC<Props> = ({ bukuId }) => {
+  const [buku, setBuku] = useState<BukuDetail | null>(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    // Fungsi untuk mengambil data dari Supabase
-    const fetchData = async () => {
+    const fetchBookData = async () => {
       try {
-        // Misalnya, untuk mengambil data buku dari tabel 'buku' di Supabase
-        const { data, error } = await supabase
+        if (!supabase) {
+          throw new Error('Supabase client is null');
+        }
+        
+        const { data: bukuData, error } = await supabase
           .from('buku')
-          .select()
-          .eq('judul', buku.judul); // Sesuaikan dengan kriteria pencarian yang diperlukan
+          .select('*')
+          .eq('id', bukuId)
+          .single();
 
         if (error) {
           throw error;
         }
 
-        // Lakukan sesuatu dengan data yang diperoleh dari Supabase
-        console.log('Data buku dari Supabase:', data);
-      } catch (error) {
-        console.error('Gagal mengambil data:', error.message);
+        if (bukuData) {
+          setBuku(bukuData);
+        }
+      } catch (error: any) {
+        console.error('Error fetching book data:', error.message || error);
       }
     };
+    fetchBookData();
+  }, [bukuId]);
 
-    // Panggil fungsi fetchData saat komponen dimount
-    fetchData();
-  }, [buku.judul]);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='font-poppins'>
-      <div>
-        <img src="/images/BackgroundLabrary.png" className='w-full h-full' alt="gambar background"/>
-      </div>
-        
-      <div className='flex pt-8 pb-4'>
-        <div className='pl-9 pr-5 text-xl flex items-center justify-between'>
-          <Link href="/">
-            <img src="/icon/BackButton.png" alt="" />
-          </Link>
-        </div>
-          <div className='text-[#426E6D] text-3xl font-bold flex items-center'>Detail Buku</div>
-        </div>
-        <div className="mx-[130px] width-screen border-0 flex flex-wrap flex-row">
-
-  {/* Bagian Detail Buku */}
-  <div className='w-[560px] h-[320px] rounded-lg border-2 border-slate-200 shadow-md mr-2 mb-3 p-5 flex'>
-    {/* Isi Detail Buku */}
-    <div className="flex flex-wrap">
-      <img src='/images/hungergames.png' className='w-[180px] h-[250px]' alt='Book cover' />
-      <div className="ml-[50px] text-[#858585]" style={{ fontSize: '18px' }}>
-        <div className="text-[#858585]" style={{ fontSize: '18px' }}>Judul Buku</div>
-        <div className="text-[#000000] font-bold" style={{ fontSize: '24px' }}>The Lord of the Rings</div>
-        <div className='flex text-[#858585]  mt-2' style={{fontSize : '18px'}}>Penulis
-            <div className='ml-[70px] mb-2 text-[#000000]'>JRR Tolkien</div></div>
-            <div className='flex' style={{fontSize : '18px'}}>Penerbit
-            <div className='ml-[58px] mb-2 text-[#000000]'>Britania Raya</div></div>
-            <div className='flex' style={{fontSize : '18px'}}>Kategori
-            <div className='ml-[57px] mb-2 text-[#000000]'>Fantasi</div></div>
-            <div className='flex' style={{fontSize : '18px'}}>Tahun Terbit
-            <div className='ml-[21px] mb-2 text-[#000000]'>1954</div></div>
-            <div className='mx-3 my-2 h-[30px] bg-[#CCFBF1] text-[#047857] flex rounded-2xl items-center justify-center'>Tersedia</div>
-      </div>
+      <div className='w-[560px] h-[320px] rounded-lg border-2 border-slate-200 shadow-md mr-2 mb-3 p-5 flex'>
+        <div className="flex flex-wrap">
+          <img src={buku?.coverbuku} className='w-[180px] h-[250px]' alt='Book cover' />
+          {buku && (
+  <div className="ml-[50px] text-[#858585]" style={{ fontSize: '18px' }}>
+    <div className="text-[#858585]" style={{ fontSize: '18px' }}>Judul Buku</div>
+    <div className="text-[#000000] font-bold" style={{ fontSize: '24px' }}>{buku.judul}</div>
+    {/* Render other book details */}
+    <div className='flex text-[#858585] mt-2' style={{ fontSize: '18px' }}>Penulis
+      <div className='ml-[70px] mb-2 text-[#000000]'>{buku.penulis}</div>
+    </div>
+    <div className='flex' style={{ fontSize: '18px' }}>Penerbit
+      <div className='ml-[58px] mb-2 text-[#000000]'>{buku.penerbit}</div>
+    </div>
+    <div className='flex' style={{ fontSize: '18px' }}>Kategori
+      <div className='ml-[57px] mb-2 text-[#000000]'>{buku.kategori}</div>
+    </div>
+    <div className='flex' style={{ fontSize: '18px' }}>Tahun Terbit
+      <div className='ml-[21px] mb-2 text-[#000000]'>{buku.tahunterbit}</div>
+    </div>
+    <div className='mx-3 my-2 h-[30px] bg-[#CCFBF1] text-[#047857] flex rounded-2xl items-center justify-center'>
+      {buku.availability}
     </div>
   </div>
-  {/* Kotak sinopsis */}
-  <div className='flex-1 px-5 rounded-lg'>
-    <div className='text-[#426E6D] font-bold' style={{ fontSize: '22px' }}>Sinopsis</div>
-    <div className='bg-[#C6E7ED] bg-opacity-50 p-3 rounded-xl mt-3 text-justify'>
-      Menyajikan epik yang menggambarkan Perang Besar Cincin, perjuangan antara yang baik dan yang jahat di Dunia Tengah, mengikuti pengembaraan Frodo si hobbit dan teman-temannya dalam upaya menghancurkan Cincin Kekuasaan.
+)}
+      {/* Kotak sinopsis */}
+        <div className='flex-1 px-5 rounded-lg'>
+          <div className='text-[#426E6D] font-bold' style={{ fontSize: '22px' }}>Sinopsis</div>
+          <div className='bg-[#C6E7ED] bg-opacity-50 p-3 rounded-xl mt-3 text-justify'>
+            Menyajikan epik yang menggambarkan Perang Besar Cincin, perjuangan antara yang baik dan yang jahat di Dunia Tengah, mengikuti pengembaraan Frodo si hobbit dan teman-temannya dalam upaya menghancurkan Cincin Kekuasaan.
+          </div>
     </div>
     <div className='mt-4 text-[#426E6D] font-bold' style={{ fontSize: '22px' }}>Ulasan terkait</div>
     <div>
-            {/* Rating Ulasan */}
-            <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
+    <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
               <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
               </svg>
@@ -104,21 +107,18 @@ const DetailBuku: React.FC<Props>= ({ buku }) => {
               <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
               </svg>
-              {/* Tambahkan ikon rating lainnya sesuai kebutuhan */}
               <div className="px-2">
                 <h3 className="ms-2 text-sm font-semibold text-gray-600 dark:text-white">Sangat Baik</h3>
               </div>
             </div>
           </div>
-    <div className='bg-[#C6E7ED] bg-opacity-50 p-3 rounded-xl mt-3 text-justify'>Buku ini sangat baik</div>
-  </div>
-</div>
-<div className='my-[50px] ml-[130px] mr-[590px] flex'>
-<button className='p-2 text-center rounded-lg ml-[200px] bg-[#426e6d] text-[#fefbfb]'>+ List Peminjaman</button>
-<div className='p-2 text-center rounded-lg ml-[30px] bg-[#c86f43] text-[#fefbfb]'>+ List Peminjaman</div></div>
-</div>
-
+          <div className='bg-[#C6E7ED] bg-opacity-50 p-3 rounded-xl mt-3 text-justify'>Buku ini sangat baik</div>
+        </div>
+      </div>
+      </div>
+    
   );
 };
 
 export default DetailBuku;
+
