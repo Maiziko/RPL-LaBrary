3d49e5bfa0f65f8bf7b7c0a9423ff2a51fce4047
Regine <18221025@std.stei.itb.ac.id>
2023-11-24 14:31:02 +0700

    ganti dikit

--- pages/koleksibuku.tsx
+++ pages/koleksibuku.tsx
@@ -9,7 +9,7 @@ interface Buku {
   penulis: string;
   deskripsi: string;
   status_ketersediaan: string;
-  coverbuku: string;
+  cover_buku: string;
   kategori: string;
 }
 type KategoriBuku = Record<string, Buku[]>;
@@ -133,7 +133,7 @@ const filteredBooks =
       <div className="book-container ml-[160px] border-0 flex flex-wrap flex-row">
       {filteredBooks.map((book, index) => (
         <div key={index} onClick={() => handleBookClick(book)} className='w-[220px] h-[400px] rounded-lg border-2 border-slate-200 shadow-md mr-4 mb-4'>
-          <img src={book.coverbuku} className='m-3 w-[160px] h-[200px]' alt={`${book.judul} Image`}/>
+          <img src={book.cover_buku} className='m-3 w-[160px] h-[200px]' alt={`${book.judul} Image`}/>
           <div className="mx-3 mb-1 font-bold overflow-hidden overflow-ellipsis" style={{ fontSize:'22px', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 1 }}>{book.judul}</div>
           <div className='mx-3 mb-1' style={{fontSize:'16px'}}>{book.penulis}</div>
           <div className="mx-3 mb-3 text-[#9E9FA1] justify-align overflow-hidden overflow-ellipsis" style={{ fontSize:'14px', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 2 }}>{book.deskripsi}</div>
